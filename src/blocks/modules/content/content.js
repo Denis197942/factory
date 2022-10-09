f6718828f2aa2a1ed232949cc01c78d803b3e03e
Denis <kokotov.denis@yandex.ru>
2022-10-09 15:18:18 +0700

    content modules

--- src/blocks/modules/content/content.js
+++ src/blocks/modules/content/content.js
@@ -1,13 +1,24 @@
 modules.define('content', ['i-bem-dom'], function(provide, bemDom) {
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
+    provide(bemDom.declBlock(this.name, {
+        onSetMod: {
+            js: {
+                inited: function() {
+                    window.addEventListener('DOMcontentLoader', function() {
+                        'use strict';
+                    
+                        let tab = document.querySelectorAll('.content__tab'),
+                            tabs = document.querySelector('.content__tabs'),
+                            tabContent = document.querySelectorAll('.content__wrap');
+                    
+                        console.log(tab);
+                    });
+                }
             }
         }
-    }
-}));
+    }));
+    
+    });
 
-});
+
+
